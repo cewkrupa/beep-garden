05179133a668f988122827cdd462746842dd3b6d
Elliott Krupa <c.e.w.krupa@outlook.com>
2022-06-29 22:28:55 -0400

    Add scale selector

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
    class="full-width background"
    @click.prevent="beepOnClick"
    >
-    <h1 id="title">Beep Garden</h1>
+    <HeaderControl v-model="selectedScale" :options="availableScaleNames"/>
     <img id="foreground" :src="foregrounds[0].src" :alt="foregrounds[0].alt" :height="height" :width="width" />
   </div>
 </template>
@@ -11,23 +11,38 @@
 <script>
 import GardenGates from './assets/garden-gates-entrance.png'
 import Pizzicato from 'pizzicato'
+import HeaderControl from './HeaderControl'
 
 export default {
   name: 'App',
+  components: {
+    HeaderControl
+  },
   data: function () {
     return {
       height: 0,
       width: 0,
       x: 0,
       y: 0,
-      gMinorPentatonic: [98, 116.54, 130.81, 146.83, 174.61, 196, 233.08, 261.63, 293.66, 349.23, 392, 466.16, 523.25, 587.33, 698.46, 783.99, 932.33, 1046.5, 1174.66, 1396.91, 1567.98],
-      gMajorPentatonic: [98, 110, 123.47, 146.83, 164.81, 196, 220, 246.94, 293.66, 329.63, 392, 440, 493.88, 587.33, 659.25, 783.99, 880, 987.77, 1174.66, 1318.51, 1567.98],
+      availableScales: {
+        gMinorPentatonic: [98, 116.54, 130.81, 146.83, 174.61, 196, 233.08, 261.63, 293.66, 349.23, 392, 466.16, 523.25, 587.33, 698.46, 783.99, 932.33, 1046.5, 1174.66, 1396.91, 1567.98],
+        gMajorPentatonic: [98, 110, 123.47, 146.83, 164.81, 196, 220, 246.94, 293.66, 329.63, 392, 440, 493.88, 587.33, 659.25, 783.99, 880, 987.77, 1174.66, 1318.51, 1567.98]
+      },
+      selectedScale: 'gMinorPentatonic',
       foregrounds: [
         {
           src: GardenGates,
           alt: 'A blackened sillhouette of wrought iron gates'
         }
       ]
+    }
+  },
+  computed: {
+    availableScaleNames: function () {
+      return Object.keys(this.availableScales)
+    },
+    currentScale: function () {
+      return this.availableScales[this.selectedScale]
     }
   },
   created () {
@@ -49,7 +64,7 @@ export default {
         source: 'wave',
         options: {
           release: 0.1,
-          frequency: this.gMinorPentatonic[(this.x + this.y) % this.gMinorPentatonic.length]
+          frequency: this.currentScale[(this.x + this.y) % this.currentScale.length]
         }
       })
 
@@ -86,18 +101,6 @@ export default {
 </script>
 
 <style>
-#title {
-  text-align: center;
-  position: fixed;
-  top: 3em;
-  margin: auto auto;
-  width: 100%;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  font-family: "Dosis", sans-serif;
-}
 body,
 html,
 .full-width {
---
+++ src/HeaderControl.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex fixed" id="wrapper">
+    <div id="headerControl" class="flex fixed" @click.stop>
+      <h1 @click="showSelect = !showSelect">Beep Garden</h1>
+      <select v-if="showSelect" :value="value" @change="emitChange($event.target.value)">
+        <option v-for="option in options" :value="option" :key="option">{{option}}</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderControl',
+  props: {
+    value: {
+      type: String,
+      default: 'gMinorPentatonic'
+    },
+    options: Array
+  },
+  data () {
+    return {
+      showSelect: false
+    }
+  },
+  methods: {
+    emitChange (value) {
+      this.$emit('change', value)
+      this.showSelect = false
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+.flex {
+  display: flex;
+}
+.fixed {
+  position: fixed;
+}
+#wrapper {
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+}
+#headerControl {
+  top: 3em;
+  margin: auto auto;
+  flex-direction: column;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  font-family: "Dosis", sans-serif;
+}
+</style>
